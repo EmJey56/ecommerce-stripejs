4fbf6ae8fd6c60385ec07c539ddabe974c280920
EmJey56 <139371735+EmJey56@users.noreply.github.com>
2024-12-18 17:18:28 +0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,16 +11,25 @@ const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 
-
+// Database connection
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull!"))
+  .then(() => console.log("DB Connection Successful!"))
   .catch((err) => {
     console.log(err);
   });
 
-app.use(cors());
+// Middleware for CORS
+app.use(
+  cors({
+    origin: "https://ecommerce-stripejs-client.onrender.com", // Replace with your frontend's URL
+    credentials: true, // If your requests need cookies or auth headers
+  })
+);
+
 app.use(express.json());
+
+// Routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
@@ -28,6 +37,7 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
+// Start the server
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
